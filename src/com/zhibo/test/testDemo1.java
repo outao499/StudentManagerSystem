fefc88c20988eb4fef1175539e21f310d9049093
outao <outao499@qq.com>
2025-07-10 22:44:18 +0800

    项目第一次提交

---
+++ src/com/zhibo/test/testDemo1.java
@@ -0,0 +1,14 @@
+package com.zhibo.test;
+
+/**
+ * @param
+ * @param
+ * @author 19349
+ * @description
+ * @date 2025/7/10 22:43
+ */
+public class testDemo1 {
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
+}
